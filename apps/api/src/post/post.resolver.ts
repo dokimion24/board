65af2463973eab589ec28af1bf4e6ef87c40e034
dokimion24 <leecy3610@gmail.com>
2025-04-06 21:10:37 +0900

    feat: add post resource

---
+++ apps/api/src/post/post.resolver.ts
@@ -0,0 +1,13 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { Post } from './entities/post.entity';
+import { PostService } from './post.service';
+
+@Resolver(() => Post)
+export class PostResolver {
+  constructor(private readonly postService: PostService) {}
+
+  @Query(() => [Post], { name: 'posts' })
+  findAll() {
+    return this.postService.findAll();
+  }
+}
