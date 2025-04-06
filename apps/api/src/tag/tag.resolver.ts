b39dbbc5f1ea79874778c2e2259817921cdc4212
dokimion24 <leecy3610@gmail.com>
2025-04-06 21:11:09 +0900

    feat: add tag resource

---
+++ apps/api/src/tag/tag.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver } from '@nestjs/graphql';
+import { Tag } from './entities/tag.entity';
+import { TagService } from './tag.service';
+
+@Resolver(() => Tag)
+export class TagResolver {
+  constructor(private readonly tagService: TagService) {}
+}
