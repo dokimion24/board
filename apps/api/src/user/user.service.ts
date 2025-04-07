19a15ce19f792af3a643b73521094574fdf4e2b7
dokimion24 <leecy3610@gmail.com>
2025-04-07 23:34:50 +0900

    feat: add user create

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,26 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { hash } from 'argon2';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserInput } from './dto/create-user.input';
-import { UpdateUserInput } from './dto/update-user.input';
-
 @Injectable()
 export class UserService {
-  create(createUserInput: CreateUserInput) {
-    return 'This action adds a new user';
-  }
+  constructor(private readonly prisma: PrismaService) {}
 
-  findAll() {
-    return `This action returns all user`;
-  }
+  async create(createUserInput: CreateUserInput) {
+    const { password, ...user } = createUserInput;
+    const hashedPassword = await hash(password);
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return await this.prisma.user.create({
+      data: {
+        password: hashedPassword,
+        ...user,
+      },
+    });
   }
 }
