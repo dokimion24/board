d8565b58a7ab3f11fa70ec3f5d3b032f11c14e86
dokimion24 <leecy3610@gmail.com>
2025-04-07 23:31:41 +0900

    feat: add createUser grpahql mutation

--- apps/api/src/user/user.resolver.ts
+++ apps/api/src/user/user.resolver.ts
@@ -1,8 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
-
 @Resolver(() => User)
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
+
+  @Mutation(() => User)
+  async createUser(@Args('createUserInput') createUserInput: CreateUserInput) {
+    return this.userService.create(createUserInput);
+  }
 }
