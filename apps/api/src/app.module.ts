976698d29c62e098c390a1539827ae8fd6694dd6
dokimion24 <leecy3610@gmail.com>
2025-04-08 07:21:25 +0900

    feat: global config setup

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,22 +1,26 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { PostModule } from './post/post.module';
-import { UserModule } from './user/user.module';
-import { CommentModule } from './comment/comment.module';
-import { TagModule } from './tag/tag.module';
-import { LikeModule } from './like/like.module';
 import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { PostModule } from './post/post.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TagModule } from './tag/tag.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
     PrismaModule,
     PostModule,
