d9e8de0b7999b373f345d3cfcbe517e43dd2a821
dokimion24 <leecy3610@gmail.com>
2025-04-10 13:03:28 +0900

    feat: add hero component

---
+++ apps/front/src/components/hero.tsx
@@ -0,0 +1,15 @@
+export default function Hero() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="container mx-auto flex flex-col md:flex-row items-center flex-wrap px-3">
+        <div className="flex flex-col w-full justify-center items-start md:w-2/5 text-cnter md:text-left">
+          <p className="capitalize tracking-wide w-full">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+            quos.
+          </p>
+          <h2 className="my-5 text-5xl font-bold leading-tight">Weclome</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
