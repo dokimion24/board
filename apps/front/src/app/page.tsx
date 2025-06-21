84a4f8700935019e149d66ba3b94d70e48cf57f5
dokimion24 <leecy3610@gmail.com>
2025-06-21 14:49:45 +0900

    feat(front): 페이지네이션 추가 및 UI 개선

--- apps/front/src/app/page.tsx
+++ apps/front/src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero";
-import Posts from "@/components/Posts";
+import Posts from "@/components/posts";
 import { getPosts } from "@/lib/actions/postActions";
+import { DEFAULT_PAGE_SIZE } from "@/lib/constants";
 
 type Props = {
   searchParams: Promise<{
@@ -10,14 +11,18 @@ type Props = {
 
 export default async function Home({ searchParams }: Props) {
   const { page } = await searchParams;
-  const { posts } = await getPosts({
+  const { posts, totalCount } = await getPosts({
     page: page ? +page : undefined,
   });
 
   return (
     <main>
       <Hero />
-      <Posts posts={posts} />
+      <Posts
+        posts={posts}
+        currentPage={page ? +page : 1}
+        totalPages={Math.ceil(totalCount / DEFAULT_PAGE_SIZE)}
+      />
     </main>
   );
 }
--- apps/front/src/components/Posts.tsx
+++ apps/front/src/components/Posts.tsx
@@ -1,14 +1,26 @@
 import { Post } from "@/lib/types/modelTypes";
+import Pagination from "./pagination";
 import PostCard from "./post-card";
 
 interface PostsProps {
   posts: Post[];
+  totalPages: number;
+  currentPage: number;
 }
 
-export default function Posts({ posts }: PostsProps) {
+export default function Posts({ posts, totalPages, currentPage }: PostsProps) {
+  console.log(totalPages, currentPage);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {posts?.map((post) => <PostCard key={post.id} {...post} />)}
-    </div>
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+      </div>
+
+      {totalPages > 1 && (
+        <div className="mt-8">
+          <Pagination totalPages={totalPages} currentPage={currentPage} />
+        </div>
+      )}
+    </>
   );
 }
